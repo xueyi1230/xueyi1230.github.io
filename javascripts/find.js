2abb8e45f257859af514e5540b0e461ca032e115
xueyi1230 <xy910828@gmail.com>
2015-03-03 23:16:27 +0800

    样式基本修改完毕

--- javascripts/find.js
+++ javascripts/find.js
@@ -11,8 +11,11 @@ require(['jquery'],function($){
 		$(this).next("ul").slideToggle(700);
 	});
 	$("#gotop").click(function(){
-		$('body').animate({scrollTop:0},700);
+		$('html,body').animate({scrollTop:0},700);
 	});
 
-	console.log('早上好，中午好，晚上好，感谢您阅读我的简历：）');
+	//若ie则不显示问候消息
+	if(!window.ActiveXObject){
+		console.log('早上好，中午好，晚上好，感谢您阅读我的简历：）');
+	}
 });
